7b89280e88d0218e510ff89f154fa54d873e87d8
SaulRamos7 <saulivanramos75@gmail.com>
2026-07-05 03:46:32 -0600

    fix: agregar arm_state_publisher al launch file
    El modelo no se visualizaba correctamente en RViz (error en TF) porque el nodo arm_state_publisher no estaba guardado en el archivo launch. Al faltar este nodo, nadie publicaba en /joint_states, rompiendo las transformaciones de la cinemática.

--- src/arm_kinematics/launch/arm_core.launch.py
+++ src/arm_kinematics/launch/arm_core.launch.py
@@ -1,16 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     return LaunchDescription([
-        # Arranca el nodo publicador
+        # Nodo de publicacion de estados, este no lo había guardado y no jalaba
+        Node(
+            package='arm_kinematics',
+            executable='arm_state_publisher', 
+            name='arm_state_publisher_node'
+        ),
+        # 2. publicar cinemática
         Node(
             package='arm_kinematics',
             executable='fk_publisher',
             name='fk_publisher_node'
         ),
-        # Arranca el nodo suscriptor
+        # 3. el listener de angulos
         Node(
             package='arm_kinematics',
             executable='angle_listener',
