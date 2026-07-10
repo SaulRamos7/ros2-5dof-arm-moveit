caa81fee0ac26f68a626be73fc26bd2c7eaaed0c
SaulRamos7 <153052680+SaulRamos7@users.noreply.github.com>
2026-07-10 02:22:17 -0600

    Update fk_publisher.py

--- src/arm_kinematics/arm_kinematics/fk_publisher.py
+++ src/arm_kinematics/arm_kinematics/fk_publisher.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from std_msgs.msg import Float64
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
-# 1. LA HERRAMIENTA NUEVA: Importamos el formato estándar para servicios tipo "gatillo"
+#Importamos el formato estándar para servicios tipo "gatillo"
 from std_srvs.srv import Trigger 
 
 class FKPublisher(Node):
@@ -17,20 +17,20 @@ class FKPublisher(Node):
             depth=10
         )
         
-        # El Tópico (Megáfono)
+        #Tópico 
         self.publisher_ = self.create_publisher(Float64, '/arm/joint_angle', qos_profile)
         
-        # 2. EL SERVICIO (El Teléfono): Creamos un servidor esperando llamadas en la línea '/arm/reset'
+        # servicio, creamos un servidor esperando llamadas en la línea '/arm/reset'
         self.srv = self.create_service(Trigger, '/arm/reset', self.reset_callback)
         
-        # 3. Memoria interna: Guardamos el ángulo actual en la memoria RAM del nodo
+        # memoria interna guardamos el ángulo actual en la memoria RAM del nodo
         self.current_angle = 0.0 
         
         self.timer = self.create_timer(0.5, self.timer_callback)
         self.get_logger().info('Nodo FK Publisher iniciado con Servicio de Reset listo.')
 
     def timer_callback(self):
-        # Simulamos movimiento sumando un poquito al ángulo actual cada 0.5 seg
+        # simulamos movimiento sumando un poquito al ángulo actual cada 0.5 seg
         self.current_angle += random.uniform(0.05, 0.2)
         
         msg = Float64()
@@ -38,7 +38,7 @@ class FKPublisher(Node):
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publicando: {msg.data:.4f} rad')
 
-    # 4. EL CALLBACK DEL SERVICIO: Esto SOLO se ejecuta cuando alguien hace la llamada
+    #  callback del servicio Esto SOLO se ejecuta cuando alguien hace la llamada
     def reset_callback(self, request, response):
         self.get_logger().info('¡PETICIÓN RECIBIDA! Interrumpiendo operación para resetear...')
         
