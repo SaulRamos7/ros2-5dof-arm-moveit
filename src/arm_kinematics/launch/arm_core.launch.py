fecbfcbbc566a3fe84b163510ed50ff8e67b778d
SaulRamos7 <153052680+SaulRamos7@users.noreply.github.com>
2026-07-10 02:22:43 -0600

    Update arm_core.launch.py

--- src/arm_kinematics/launch/arm_core.launch.py
+++ src/arm_kinematics/launch/arm_core.launch.py
@@ -9,13 +9,13 @@ def generate_launch_description():
             executable='arm_state_publisher', 
             name='arm_state_publisher_node'
         ),
-        # 2. publicar cinemática
+        #  publicar cinemática
         Node(
             package='arm_kinematics',
             executable='fk_publisher',
             name='fk_publisher_node'
         ),
-        # 3. el listener de angulos
+        # el listener de angulos
         Node(
             package='arm_kinematics',
             executable='angle_listener',
